ae6ee05dd4b4b8dab241490013c2a1babd9f21a2
rijpma <auke.rijpma@gmail.com>
2016-06-17 16:00:03 +0200

    cleanup

--- fijnstofranges.R
+++ fijnstofranges.R
@@ -5,19 +5,16 @@ air <- data.table::fread('~/downloads/data/AirBase_v8_statistics.csv')
 ctrs <- read.csv("~/dropbox/uurabo/nwcountries.csv")
 iso2c <- countrycode::countrycode(ctrs$iso3c, 'iso3c', 'iso2c')
 ctrs$iso2c <- countrycode::countrycode(ctrs$iso3c, 'iso3c', 'iso2c')
-write.csv(ctrs, "~/dropbox/uurabo/nwcountries2.csv")
-# iso2c <- substr(ctrs$iso3c, 1, 2)
 
 table(air[grep('particulate matter < 10', air$component_name, ignore.case=T), component_name])
 
 air_pm10 <- air[grep('particulate matter < 10', air$component_name, ignore.case=T), ]
 air_pm10[, ctr:=substr(station_european_code, 1, 2)]
 air_pm10_nw <- air_pm10[ctr %in% iso2c, ]
-air_pm10_nw[, mean(statistic_value), by=list(ctr, statistics_year, statistic_name)][, range(statistics_year)]
+
 air_pm10_nw[statistic_name=="maximum", max(statistic_value), by=list(ctr, statistics_year, statistic_name)]
 air_pm10_nw[statistic_name=="annual mean", mean(statistic_value), by=list(ctr, statistics_year, statistic_name)]
 air_pm10_nw[statistic_name=="annual mean", mean(statistic_value), by=list(ctr, statistics_year, statistic_name)][, range(V1)]
-air_pm10_nw[statistic_name=="annual mean", mean(statistic_value), by=list(ctr, statistics_year, statistic_name)][, range(statistics_year)]
 
 mineur <- min(air_pm10_nw[statistic_name=="annual mean" & statistics_year > 2002, min(statistic_value), by=list(ctr, statistics_year, statistic_name)][, range(V1)])
 maxeur <- max(air_pm10_nw[statistic_name=="annual mean" & statistics_year > 2002, max(statistic_value), by=list(ctr, statistics_year, statistic_name)][, range(V1)])
